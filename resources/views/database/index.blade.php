6990eb1d27d7e25773dfea97cedaaf68cfed33d1
Jose Coelho <camilocoelho93@gmail.com>
2017-01-17 23:45:09 +0100

    I/E page update

--- resources/views/database/index.blade.php
+++ resources/views/database/index.blade.php
@@ -6,7 +6,7 @@
 
 @section('breadcrums')
 	<span class="cst-bc-chevron">></span>
-	<a class="cst-white-link cst-breadcrum" href="{{route('databases')}}">Databases</a>
+	<a class="cst-white-link cst-breadcrum" href="{{route('databases')}}">databases</a>
 	<span class="cst-bc-chevron">></span>
 	<a class="cst-white-link cst-breadcrum" href="{{route('show-database',['name' => $name])}}">{{$name}}</a>
 @endsection
@@ -15,7 +15,7 @@
 	<div class="mdl-layout__tab-bar mdl-js-ripple-effect">
 		<a href="#tables-page" class="mdl-layout__tab is-active">Tables</a>
 		<a href="#sql-page" class="mdl-layout__tab">SQL</a>
-		<a href="#io-page" class="mdl-layout__tab">Import/Export</a>
+		<a href="#ie-page" class="mdl-layout__tab">Import/Export</a>
 		<a href="#edit-page" class="mdl-layout__tab">Edit</a>
 		<a href="#privileges-page" class="mdl-layout__tab">Privileges</a>
 	</div>
@@ -59,7 +59,7 @@
 
 		</div>
 	</section>
-	<section class="mdl-layout__tab-panel" id="io-page">
+	<section class="mdl-layout__tab-panel" id="ie-page">
 		<div class="page-content">
 
 		</div>
